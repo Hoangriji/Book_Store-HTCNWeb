ca589b5001ebc95ec1e2b3ad07bbce338bbe6999
Hoangriji <chunghoang26826@gmail.com>
2026-02-07 12:20:10 +0700

    Fix: change code layout css -> bs5

--- assets/shared/js/components/footer.js
+++ assets/shared/js/components/footer.js
@@ -3,21 +3,21 @@ export const footer = () => {
   const footerTemplate = /* html */ `
         <div class="container">
             <div class="footer-wrapper">
-                <div class="footer-content">
-                    <div class="footer-section">
+                <div class="row g-4 footer-content">
+                    <div class="col-12 col-md-6 col-lg-5 footer-section">
                         <img src="assets/imgs/homePage/logo.png" alt="">
                         <h4 class="footer-section-title">FiveH Book Store</h4>
                         <p>Your one-stop shop for all your reading needs. Discover, explore, and indulge in the world of books with us!</p>
                     </div>
 
-                    <div class="footer-section">
+                    <div class="col-12 col-md-6 col-lg footer-section">
                         <h4 class="footer-section-title">Contact Us</h4>
                         <p>Email: contact@bookstore.com</p>
                         <p>Phone: +1 234 567 890</p>
                         <p>Address: 123 Book St, City, Country</p>
                     </div>
 
-                    <div class="footer-section">
+                    <div class="col-12 col-md-6 col-lg footer-section">
                         <h4 class="footer-section-title">Follow Us</h4>
                         <div class="social-icons">
                             <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
@@ -26,7 +26,7 @@ export const footer = () => {
                         </div>
                     </div>
 
-                    <div class="footer-section">
+                    <div class="col-12 col-md-6 col-lg footer-section">
                         <h4 class="footer-section-title">Quick Links</h4>
                         <ul>
                             <li><i class="fa-solid fa-arrow-right"></i><a href="index.html">Home</a></li>
