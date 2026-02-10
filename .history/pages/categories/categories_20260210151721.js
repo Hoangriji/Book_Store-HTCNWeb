adb8ac082b2eeeb4ee334a1ef670ce5ad388d47f
trunghau06 <hau.121747@gmail.com>
2026-02-10 16:16:22 +0700

    feat: complete categories page and update header/footer

---
+++ .history/pages/categories/categories_20260210151721.js
@@ -0,0 +1,91 @@
+const books = [
+    {
+        image: "/imgs/categories/productBook1.png",
+        badge: "MỚI",
+        title: "Thunmanhandiya",
+        description: "Mahagamasekara",
+        price: 129
+    },
+    {
+        image: "/imgs/categories/productBook2.png",
+        badge: "SALE",
+        title: "Modern Science",
+        description: "John Carter",
+        price: 159
+    },
+    {
+        image: "/imgs/categories/productBook3.png",
+        badge: "",
+        title: "Economic World",
+        description: "Adam Smith",
+        price: 75
+    },
+];
+
+const minInput = document.getElementById("minPrice");
+const maxInput = document.getElementById("maxPrice");
+const rangeText = document.getElementById("priceRangeText");
+
+const progress = document.createElement("div");
+progress.classList.add("range-progress");
+document.querySelector(".range-slider").appendChild(progress);
+
+function renderBooks(bookList) {
+    const booksGrid = document.getElementById("booksGrid");
+    booksGrid.innerHTML = "";
+
+    bookList.forEach(book => {
+        booksGrid.innerHTML += `
+            <div class="col-auto">
+                <div class="book-card">
+                    <div class="book-card__image-wrapper">
+                        <img src="${book.image}" class="book-card__image">
+                        ${book.badge ? `<span class="book-card__badge">${book.badge}</span>` : ""}
+                    </div>
+
+                    <div class="book-card__body">
+                        <div class="book-card__content">
+                            <h4 class="book-card__title">${book.title}</h4>
+                            <p class="book-card__description">${book.description}</p>
+                            <div class="book-card__price-wrapper">
+                                <span class="book-card__price">${book.price}.000 đ</span>
+                            </div>
+                        </div>
+
+                        <div class="book-card__actions">
+                            <button class="btn btn-primary book-card__detail-btn">
+                                Xem chi tiết
+                            </button>
+
+                            <button class="book-card__cart-btn">
+                                <i class="fas fa-shopping-cart"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+
+    document.querySelector(".products-header__result").textContent =
+        `Hiển thị ${bookList.length} trong ${books.length} kết quả`;
+}
+
+function updateRange() {
+    let min = parseInt(minInput.value);
+    let max = parseInt(maxInput.value);
+
+    const percentMin = (min / minInput.max) * 100;
+    const percentMax = (max / maxInput.max) * 100;
+
+    progress.style.left = percentMin + "%";
+    progress.style.width = (percentMax - percentMin) + "%";
+
+    rangeText.textContent = `$${min} - $${max}`;
+}
+
+minInput.addEventListener("input", updateRange);
+maxInput.addEventListener("input", updateRange);
+
+updateRange();
+renderBooks(books);
---
+++ .history/shared/js/components/footer_20260209174804.js
@@ -0,0 +1,51 @@
+export const footer = () => {
+  const footerElement = document.querySelector(".inner-footer");
+  const footerTemplate = /* html */ `
+        <div class="container">
+            <div class="footer-wrapper">
+                <div class="row g-4 footer-content">
+                    <div class="col-12 col-md-6 col-lg-5 footer-section">
+                        <img src="imgs/homePage/logo.png" alt="">
+                        <h4 class="footer-section-title">FiveH Book Store</h4>
+                        <p>Your one-stop shop for all your reading needs. Discover, explore, and indulge in the world of books with us!</p>
+                    </div>
+
+                    <div class="col-12 col-md-6 col-lg footer-section">
+                        <h4 class="footer-section-title">Contact Us</h4>
+                        <p>Email: contact@bookstore.com</p>
+                        <p>Phone: +1 234 567 890</p>
+                        <p>Address: 123 Book St, City, Country</p>
+                    </div>
+
+                    <div class="col-12 col-md-6 col-lg footer-section">
+                        <h4 class="footer-section-title">Follow Us</h4>
+                        <div class="social-icons">
+                            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-6 col-lg footer-section">
+                        <h4 class="footer-section-title">Quick Links</h4>
+                        <ul>
+                            <li><i class="fa-solid fa-arrow-right"></i><a href="index.html">Home</a></li>
+                            <li><i class="fa-solid fa-arrow-right"></i><a href="#">Categories</a></li>
+                            <li><i class="fa-solid fa-arrow-right"></i><a href="#">New releases</a></li>
+                            <li><i class="fa-solid fa-arrow-right"></i><a href="#">About Us</a></li>
+                            <li><i class="fa-solid fa-arrow-right"></i><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="footer-bottom">
+                    <p>&copy; 2024 FiveH Book Store. All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+    `;
+
+  if (footerElement) {
+    footerElement.innerHTML = footerTemplate;
+  }
+};
